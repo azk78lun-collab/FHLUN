a7162871fde3ddab3b9fddb9c2348901d3e7bcb1
azk78lun-collab <azk78lun-collab@users.noreply.github.com>
2026-07-09 13:22:36 +0000

    fix(locale): use C.UTF-8 to eliminate 'Setting locale failed' warning
    - lun.sh: LANG=en_US.UTF-8 -> LC_ALL/LANG/LANGUAGE=C.UTF-8
    - container/nodejs/start.sh: same locale fix
    - C.UTF-8 is always available (glibc/musl busybox), avoids the warning on
      minimal systems where en_US.UTF-8 locale is not generated

--- container/nodejs/start.sh
+++ container/nodejs/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-export LANG=en_US.UTF-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export LANGUAGE=C.UTF-8
 export uuid=${uuid}
 export vlpt=${vlpt}
 export vmpt=${vmpt}
--- lun.sh
+++ lun.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-export LANG=en_US.UTF-8
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+export LANGUAGE=C.UTF-8
 [ -z "${vlpt+x}" ] || vlp=yes
 [ -z "${vmpt+x}" ] || { vmp=yes; vmag=yes; }
 [ -z "${vwpt+x}" ] || { vwp=yes; vmag=yes; }
